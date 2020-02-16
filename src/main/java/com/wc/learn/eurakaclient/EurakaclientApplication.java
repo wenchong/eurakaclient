7892d61384b04064f7faf7bf0bf19bfd9c61099c
wenchong <990632303@qq.com>
2020-02-16 23:20:03 +0800

    创建client项目

--- src/main/java/com/wc/learn/eurakaclient/EurakaclientApplication.java
+++ src/main/java/com/wc/learn/eurakaclient/EurakaclientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EurakaclientApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurakaclientApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurakaclientApplication.class, args);
+	}
 
 }
